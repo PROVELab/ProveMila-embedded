7db347a71fc6950d35fe40c767d3f9f2cc359b75
Lin Knudsen <onlineknudsen@gmail.com>
2022-12-07 15:03:50 -0800

    Add MPU calibration method

--- src/accelerometer/main.cpp
+++ src/accelerometer/main.cpp
@@ -2,21 +2,63 @@
 #include <Wire.h>
 #include "mpu6050.h"
 
-#define MPU_ACCEL_RANGE_LSB MPU_ACCEL_RANGE_8G_LSB
+#define MPU_ACCEL_RANGE MPU_ACCEL_RANGE_2G
+#define MPU_ACCEL_RANGE_LSB MPU_ACCEL_RANGE_2G_LSB
 
-void setup()
+int16_t xAxisOffset;
+int16_t yAxisOffset;
+int16_t zAxisOffset;
+
+// Assumes accelerometer sitting on flat surface, +z side up (1 G on Z axis)
+// Calculates offsets
+void calibrate()
 {
-    Serial.begin(9600);
-    Wire.begin();
-    Wire.setClock(400E3);
-    mpuStart();
-    mpuSetAccelRange(MPU_ACCEL_RANGE_8G);
+    Serial.println("Calibrating...");
+    int16_t xAxisRaw;
+    int16_t yAxisRaw;
+    int16_t zAxisRaw;
 
+    unsigned long time;
+
+    signed long xAxisRawSum = 0;
+    signed long yAxisRawSum = 0;
+    signed long zAxisRawSum = 0;
+
+    // Get average value
+    for (int i = 0; i < 200; i++)
+    {
+        // Skip first 100 samples
+        if (i < 100)
+            continue;
+
+        mpuRead3AccelAxesRaw(&xAxisRaw, &yAxisRaw, &zAxisRaw, &time);
+        xAxisRawSum += xAxisRaw;
+        yAxisRawSum += yAxisRaw;
+        zAxisRawSum += zAxisRaw;
+
+    }
+
+    int16_t xAxisAverage = xAxisRawSum / 100;
+    int16_t yAxisAverage = yAxisRawSum / 100;
+    int16_t zAxisAverage = zAxisRawSum / 100;
+
+    xAxisOffset = -xAxisAverage;
+    yAxisOffset = -yAxisAverage;
+    zAxisOffset = MPU_ACCEL_RANGE_LSB - zAxisAverage;
+
+    Serial.print("X Axis Offset: ");
+    Serial.println(xAxisOffset);
+
+    Serial.print("Y Axis Offset: ");
+    Serial.println(yAxisOffset);
+
+    Serial.print("Z Axis Offset: ");
+    Serial.println(zAxisOffset);
 }
 
-void loop()
+// Prints out raw values and calibrated values
+void debugSampleWithCalibration()
 {
-
     int16_t xAxisRaw;
     int16_t yAxisRaw;
     int16_t zAxisRaw;
@@ -25,26 +67,61 @@ void loop()
 
     mpuRead3AccelAxesRaw(&xAxisRaw, &yAxisRaw, &zAxisRaw, &time);
 
-    float xAxisConv = (float) xAxisRaw / MPU_ACCEL_RANGE_LSB;
-    float yAxisConv = (float) yAxisRaw / MPU_ACCEL_RANGE_LSB;
-    float zAxisConv = (float) zAxisRaw / MPU_ACCEL_RANGE_LSB;
+    int16_t xAxisRawCal = xAxisRaw + xAxisOffset;
+    int16_t yAxisRawCal = yAxisRaw + yAxisOffset;
+    int16_t zAxisRawCal = zAxisRaw + zAxisOffset;
 
-    Serial.print("X Axis (raw, converted): ");
+    float xAxisGNoCal = (float) xAxisRaw / MPU_ACCEL_RANGE_LSB;
+    float yAxisGNoCal = (float) yAxisRaw / MPU_ACCEL_RANGE_LSB;
+    float zAxisGNoCal = (float) zAxisRaw / MPU_ACCEL_RANGE_LSB;
+
+    float xAxisGCal = (float) xAxisRawCal / MPU_ACCEL_RANGE_LSB;
+    float yAxisGCal = (float) yAxisRawCal / MPU_ACCEL_RANGE_LSB;
+    float zAxisGCal = (float) zAxisRawCal / MPU_ACCEL_RANGE_LSB;
+
+    Serial.print("X Axis (raw no cal, G no cal, raw cal, G cal): ");
     Serial.print(xAxisRaw);
     Serial.print(", ");
-    Serial.println(xAxisConv, 2);
+    Serial.print(xAxisGNoCal, 2);
+    Serial.print(", ");
+    Serial.print(xAxisRawCal);
+    Serial.print(", ");
+    Serial.println(xAxisGCal);
 
-    Serial.print("Y Axis (raw, converted): ");
+    Serial.print("Y Axis (raw no cal, G no cal, raw cal, G cal): ");
     Serial.print(yAxisRaw);
     Serial.print(", ");
-    Serial.println(yAxisConv, 2);
+    Serial.print(yAxisGNoCal, 2);
+    Serial.print(", ");
+    Serial.print(yAxisRawCal);
+    Serial.print(", ");
+    Serial.println(yAxisGCal);
 
-    Serial.print("Z Axis (raw, converted): ");
+    Serial.print("Z Axis (raw no cal, G no cal, raw cal, G cal): ");
     Serial.print(zAxisRaw);
     Serial.print(", ");
-    Serial.println(zAxisConv, 2);
+    Serial.print(zAxisGNoCal, 2);
+    Serial.print(", ");
+    Serial.print(zAxisRawCal);
+    Serial.print(", ");
+    Serial.println(zAxisGCal);
 
     Serial.println("");
+}
 
-    delay(1000);
+void setup()
+{
+    Serial.begin(9600);
+    Wire.begin();
+    Wire.setClock(400E3);
+    mpuStart();
+    mpuSetAccelRange(MPU_ACCEL_RANGE);
+    delay(100);
+    calibrate();
+}
+
+void loop()
+{
+    // debugSampleWithCalibration();
+    delay(3000);
 }
