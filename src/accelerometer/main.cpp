5948a43e6f5fac42b58fdfa151751a9b01cfce10
Lin Knudsen <onlineknudsen@gmail.com>
2022-12-11 11:30:48 -0800

    Add parameter definitions

--- src/accelerometer/main.cpp
+++ src/accelerometer/main.cpp
@@ -2,12 +2,24 @@
 #include <Wire.h>
 #include "mpu6050.h"
 
+// ==== Test Parameters ====
+#define SAMPLE_RATE_HZ 500UL
+#define SAMPLE_DELAY_MS 10UL * 1000UL
+#define SAMPLE_WINDOW_MS 30UL * 1000UL
+#define SAMPLE_PERIOD_MS 1000UL / SAMPLE_PERIOD_HZ
+#define N_DECIMALS 3
+
+// ==== Calibration Parameters ====
+#define X_ACCEL_OFFSET 0
+#define Y_ACCEL_OFFSET 0
+#define Z_ACCEL_OFFSET 0
+
+// ==== SD Parameters ====
+#define SD_CS 10
+
+// ==== MPU Parameters ====
 #define MPU_ACCEL_RANGE MPU_ACCEL_RANGE_2G
 #define MPU_ACCEL_RANGE_LSB MPU_ACCEL_RANGE_2G_LSB
-
-int16_t xAxisOffset;
-int16_t yAxisOffset;
-int16_t zAxisOffset;
 
 void setup()
 {
