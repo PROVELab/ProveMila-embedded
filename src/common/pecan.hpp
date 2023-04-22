d76c77912db50286109594445da64defa692a596
Shynn Lawrence <shynn.lawrence@gmail.com>
2023-04-22 15:18:03 -0700

    Starting work on PTypes

--- src/common/pecan.hpp
+++ src/common/pecan.hpp
@@ -1,34 +1,5 @@
 #ifndef PECAN_H
 #define PECAN_H
-#include <stdio.h>
-
-// DEFINITIONS, TYPEDEFS
-typedef char byte;
-
-
-template <typename T>
-void fillBuf(byte * b, T value){
-    union {
-        T value;
-        byte buffer[sizeof(T)];
-    } convert_o_tron;
-    convert_o_tron.value = value;
-
-    for (int i = sizeof(T)-1; i >= 0; i--){
-        b[i] = convert_o_tron.buffer[i];
-    }
-}
-
-template <typename T>
-void unFillBuf(byte * b, T * valRef){
-    union {
-        T value;
-        byte buffer[sizeof(T)];
-    } convert_o_tron;
-    for (int i = 0; i < sizeof(T); i++){
-        convert_o_tron.buffer[i] = b[i];
-    }
-    *valRef = convert_o_tron.value;
-}
+#include "ptypes.hpp"
 
 #endif
---
+++ src/common/ptypes.hpp
@@ -0,0 +1,32 @@
+#ifndef PTYPES_H
+#define PTYPES_H
+
+// DEFINITIONS, TYPEDEFS
+typedef char byte;
+
+template <typename T>
+void fillBuf(byte * b, T value){
+    union {
+        T value;
+        byte buffer[sizeof(T)];
+    } convert_o_tron;
+    convert_o_tron.value = value;
+
+    for (int i = sizeof(T)-1; i >= 0; i--){
+        b[i] = convert_o_tron.buffer[i];
+    }
+}
+
+template <typename T>
+void unFillBuf(byte * b, T * valRef){
+    union {
+        T value;
+        byte buffer[sizeof(T)];
+    } convert_o_tron;
+    for (int i = 0; i < sizeof(T); i++){
+        convert_o_tron.buffer[i] = b[i];
+    }
+    *valRef = convert_o_tron.value;
+}
+
+#endif
