6d48de7d94598820bdac53417afdd64bd43c8d6c
spruha <spruhanayak2004@gmail.com>
2024-01-19 17:50:49 -0800

    spruart 2.0

--- src/mcu_node/main.cpp
+++ src/mcu_node/main.cpp
@@ -2,6 +2,8 @@
 #include "USBSerial.h"
 #include "uart_gen.hpp"
 
+uint32_t send_packet(Sensor_Data& packet);
+
 //create mouse object
 USBSerial serial;
 
@@ -14,13 +16,18 @@ int main(){
 
 
     while (1) {
-        spruart_packet.TP4_data++; spruart_packet.TP1_data++; spruart_packet.TP2_data++; spruart_packet.TP3_data++;
-
-        uint8_t buf[HEADER_SIZE_BYTES + MAX_DATA_SIZE_BYTES + CRC_SIZE_BYTES] = {0};
-        int out = generate_header(spruart_packet, buf);
-        int data_size = emplace(spruart_packet, buf + HEADER_SIZE_BYTES);
-        generate_CRC(buf, HEADER_SIZE_BYTES + data_size, &buf[HEADER_SIZE_BYTES + data_size ]);
-
-        serial.write(buf, HEADER_SIZE_BYTES + data_size + CRC_SIZE_BYTES);
+        send_packet(spruart_packet);
     }
+}
+
+uint32_t send_packet(Sensor_Data& packet){
+    
+    packet.TP4_data++; packet.TP1_data++; packet.TP2_data++; packet.TP3_data++;
+
+    uint8_t buf[HEADER_SIZE_BYTES + MAX_DATA_SIZE_BYTES + CRC_SIZE_BYTES] = {0};
+    generate_header(packet, buf);
+    int data_size = emplace(packet, buf + HEADER_SIZE_BYTES);
+    generate_CRC(buf, HEADER_SIZE_BYTES + data_size, &buf[HEADER_SIZE_BYTES + data_size ]);
+
+    serial.write(buf, HEADER_SIZE_BYTES + data_size + CRC_SIZE_BYTES);
 }
