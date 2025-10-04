a84f6a1b729a062b62558612181c6a0befd996b1
Shynn Lawrence <shynn.lawrence@gmail.com>
2025-10-04 11:25:19 -0700

    apply formatted code

--- src/arduinoSched/arduinoSched.hpp
+++ src/arduinoSched/arduinoSched.hpp
@@ -5,26 +5,26 @@
 struct PTask {
     void (*function)(void); // Function to call
     int16_t delay = 0;      // Milliseconds from start before a task runs
-    int16_t interval; // Milliseconds between task runs, a negative interval
-                      // indicates a task occruing one time, -interval ms after
-                      // its declared
-    int16_t location; // Location of Task in task array, assigned upon task
-                      // scheduling
-    bool locked;      // Lock CAN - Only applicable to multithreading
+    int16_t interval;       // Milliseconds between task runs, a negative interval
+                            // indicates a task occruing one time, -interval ms after
+                            // its declared
+    int16_t location;       // Location of Task in task array, assigned upon task
+                            // scheduling
+    bool locked;            // Lock CAN - Only applicable to multithreading
 };
 
 /* "Scheduler/TaskManager" */
 class PScheduler {
   private:
     int16_t ctr = 0; // Counts how many events are in queue
-    // PTask tasks[MAX_TASK_COUNT];    //delete wen decide not to use originally
-    // scheduling int16_t dCtr=0;
+                     // PTask tasks[MAX_TASK_COUNT];    //delete wen decide not to use originally
+                     // scheduling int16_t dCtr=0;
 
   public:
-    int scheduleTask(PTask *t);
+    int scheduleTask(PTask* t);
 
     // use the returned int to reference this task in runOneTimeTask
-    int scheduleOneTimeTask(PTask *t);
+    int scheduleOneTimeTask(PTask* t);
 
     void runOneTimeTask(int task, int delay);
 
@@ -32,7 +32,7 @@ class PScheduler {
 
     void changeIterations(int task, int numtIterations); //-1 for set forever
 
-    void mainloop(PCANListenParamsCollection *listens);
+    void mainloop(PCANListenParamsCollection* listens);
 
     PScheduler();
     /* Add the task to the task queue (will all be enabled
