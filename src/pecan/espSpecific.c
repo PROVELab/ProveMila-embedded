532da0b2cdaaa992f41a71d273dda32f67c59063
Torrey0 <tzaches@calpoly.edu>
2025-10-25 16:05:29 -0700

    -

--- src/pecan/espSpecific.c
+++ src/pecan/espSpecific.c
@@ -77,8 +77,8 @@ void pecan_CanInit(pecanInit config){
 
 	//Initialize configuration structures using macro initializers
     //TWAI_MODE_NORMAL
-    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(txPin, rxPin, TWAI_MODE_NO_ACK); //TWAI_MODE_NORMAL for standard behavior  
-	// twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(txPin, rxPin, TWAI_MODE_NORMAL); //TWAI_MODE_NORMAL for standard behavior  
+    // twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(txPin, rxPin, TWAI_MODE_NO_ACK); //TWAI_MODE_NORMAL for standard behavior  
+	twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(txPin, rxPin, TWAI_MODE_NORMAL); //TWAI_MODE_NORMAL for standard behavior  
 	twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
 	twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 	//Install TWAI driver
